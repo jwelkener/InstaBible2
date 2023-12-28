04690be0b0729af6180c098fe4910fa93a790aec
jwelkener <56115991+jwelkener@users.noreply.github.com>
2023-12-28 04:33:07 -0600

    create database

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # app.py
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import requests
-from sqlalchemy import func
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///instabible'  # Use SQLite for simplicity
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://johnwelkener:Luge12345@localhost:5432/instabible'
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -22,15 +21,12 @@ class Verse(db.Model):
 with app.app_context():
     db.create_all()
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route to display a random verse
 @app.route('/random')
 def display_random_verse():
-    # Get a random verse from the database
     try:
         api_url = 'https://bible-api.com/?random=verse'
         response = requests.get(api_url)
@@ -42,42 +38,3 @@ def display_random_verse():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-def home():
-    # Check if the user has submitted the form
-    if request.method == 'POST':
-        # Get the user-input verse from the form
-        user_input_verse = request.form.get('verseInput')
-
-        # Fetch the verse based on the user input
-        verse_data = get_verse_by_input(user_input_verse)
-
-        # Get the verse text
-        verse_text = verse_data['text']
-
-        # Count the number of words in the verse
-        word_count = len(verse_text.split())
-
-        # Determine the CSS class based on the word count
-        css_class = get_css_class(word_count)
-
-        return render_template('index.html', verse=verse_text, css_class=css_class)
-
-    # Render the initial page
-    return render_template('index.html', verse='', css_class='')
-
-def get_verse_by_input(user_input_verse):
-    # Fetch the verse from an external API or source based on user input
-    api_url = f'https://bible-api.com/{user_input_verse}?verse_numbers=true'
-    response = requests.get(api_url)
-    return response.json()
-
-def get_css_class(word_count):
-    if word_count <= 20:
-        return 'short'
-    elif 20 < word_count <= 50:
-        return 'medium'
-    elif 50 < word_count <= 70:
-        return 'long'
-    else:
-        return 'extra-long'
